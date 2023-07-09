11efc578015f4b42769d30131fbda072e43f70f2
Mateusz Jakubowski <mateusz.jakubowski@swmansion.com>
2023-07-09 19:56:08 +0200

    created conference_room package

--- www/settings.py
+++ www/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # our packages
+    'www.conference_room'
+
 ]
 
 MIDDLEWARE = [
